65ad0b06686462413ad9601f9039f44343502a6d
asd1510 <atharvadave000@gmail.com>
2022-05-09 19:20:17 +0530

    Topbar changes

--- frontend/src/component/topbar/topbar.js
+++ frontend/src/component/topbar/topbar.js
@@ -10,41 +10,6 @@ import BouncingBall from '../animation/BouncingBall';
 
 export default function Topbar() {
 
-  // if (otpCookie != null) {
-  //   if (otpCookie == "true") {
-  //     openDashboard = true;
-  //   }
-  //   console.log("exist");
-  // }
-
-  // useEffect(() => {
-  //   setLoading(true);
-    
-  //   if (otpCookie != null) {
-  //     if (otpCookie == "true") {
-  //       openDashboard = false;
-  //       console.log(openDashboard);
-        
-  //     }
-  //     // const check = () => {
-  //     //   return <Link to={ `/admin/dashboard/${isAuth().user._id}`} style={{ textDecoration:'none', color: 'white'}}><Person /></Link>
-  //     // }
-  //     // check();
-  //     // console.log("exist");
-  //     // value = check();
-  //   }
-
-  //   setLoading(false);
-    
-  // }, []);
-
-  // if (otpCookie != null){
-  //   console.log("otpcookie",typeof otpCookie);
-  //   if (otpCookie == "true"){
-  //     setDisplay(true);
-  //   }
-  // }
-
   return (
     <div className="topbarContainer">
       <div className="topbarLeft">
@@ -57,8 +22,7 @@ export default function Topbar() {
           <div className="topbarLinks">
               <Link to={`/disease/prediction/`} className="topbarLink">Disease Prediction</Link>
               <Link to={`/crop/prediction/`} className="topbarLink">Crop Prediction</Link>
-              <Link to={`/weather/prediction`} className="topbarLink">Weather Prediction</Link>
-              <a href="https://weather-forecast-corp.herokuapp.com/" target="_blank" className="topbarLink">Weather Prediction</a>
+              <Link to={`/weather/forecast`} className="topbarLink">Weather Prediction</Link>
               <Link to={`/cost/prediction`} className="topbarLink">Cost Prediction</Link>
 
               {!isAuth() && (
